b9688d21c95c945a6cea8c0d5c77e4bd4525a960
Rúbem Vieira <rubemvn17@gmail.com>
2023-04-03 22:24:14 -0300

    Funcionalidade de digitar numeros adicionada

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,46 @@
 let display = document.getElementById("display");
 
 
-function enterNumber(value){
-    console.log(display.value)
-    let valor = value;
+// COLOCA O FOCO PARA O INPUT ASSIM QUE A PAGINA É CARREGADA
+window.onload = function () {
+    display.focus();
+}
 
-
-    console.log(valor)
-    if(display.value != 0){
-        let newValue = parseInt(`${display.value}${valor}`)
-        display.value = newValue;
-        
-    }if(display.value == 0){
-        display.value = valor;
+// ESSA FUNÇÃO IMPEDE QUE TECLAS QUE NÃO CORRESPONDAM COM A CALCULADORA 
+document.addEventListener("keydown", function (e) {
+    if (!checkChar(e)) {
+        e.preventDefault();
     }
+})
+
+
+// IMPEDE QUE O FOCO SAIA DO INPUT
+display.addEventListener("blur", function () {
+    display.focus();
+})
+
+
+// FAZ A CHECAGEM SE O CARACTERE É UMA TECLA VÁLIDA
+function checkChar(e) {
+    const char = e.key
+    console.log(e.key)
+
+    const numbers = '[0-9"Arrow""Backspace"]';
+
+    if (char.match(numbers)) {
+        console.log(char)
+        return true
+    }
+}
+
+// DIGITA O NÚMERO SELECIONADO NA CALCULADORA
+function enterNumber(value) {
+    display.focus();
+
+    display.value += `${value}`;
+    let evento = new Event("keyup");
+    display.dispatchEvent(evento);
+    
 
 }
+
