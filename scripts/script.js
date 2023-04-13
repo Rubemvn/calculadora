f285c619c93680b269c1f39c16de209d81a77db3
Rúbem Vieira <rubemvn17@gmail.com>
2023-04-12 22:06:44 -0300

    Funções foram alteradas

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
+// IMPEDE QUE O FOCO SAIA DO INPUT
+// display.addEventListener("blur", function () {
+//     display.focus();
+// })
+// // COLOCA O FOCO PARA O INPUT ASSIM QUE A PAGINA É CARREGADA
+// window.onload = function () {
+//     display.focus();
+// }
+
+
+
 let display = document.getElementById("display");
 
 let values = {
@@ -7,63 +18,92 @@ let values = {
     valueOperation: null,
 }
 
-// COLOCA O FOCO PARA O INPUT ASSIM QUE A PAGINA É CARREGADA
-window.onload = function () {
-    display.focus();
-}
-
-
-// ESSA FUNÇÃO IMPEDE QUE TECLAS QUE NÃO CORRESPONDAM COM A CALCULADORA 
+// ESSA FUNÇÃO IMPEDE QUE TECLAS QUE NÃO CORRESPONDAM COM A CALCULADORA SEJAM DIGITADAS
 document.addEventListener("keydown", function (e) {
-    if (!checkChar(e)) {
-        e.preventDefault();
-    } if (e.key === "Escape") {
+    if (e.key === "Escape") {
         reset();
-
     } if (e.key === "Backspace") {
         backspace();
+    } if (e.key === "Enter") {
+        resolveOperation();
     }
 
+    let numberKey = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "."];
+    let operations = ["+", "-", "*", "/"]
 
-
+    let valueBoolean = false;
+    for (let i = 0; i <= numberKey.length; i++) {
+        if (e.key == numberKey[i]) {
+            enterNumber(e.key);
+            valueBoolean = true;
+            break;
+        }
+    } if (!valueBoolean) {
+        for (let i = 0; i < operations.length; i++) {
+            if (e.key == operations[i]) {
+                enterOperation(e.key);
+                break;
+            }
+        }
+    }
 })
-
-
-// IMPEDE QUE O FOCO SAIA DO INPUT
-display.addEventListener("blur", function () {
-    display.focus();
-})
-
-
 // FAZ A CHECAGEM SE O CARACTERE É UMA TECLA VÁLIDA
 function checkChar(e) {
-    const char = e.key
     // console.log(char)
 
-    const numbers = '[0-9]';
-
-    if (char.match(numbers) || char == "ArrowLeft" || char == "ArrowRight") {
-        return true
-    }
-}
-
-
-
-
-
-function enterOperation(op){
-    formatDisplay(op)
-    if(typeof parseFloat(display.value) === "number"){
-        valueChecker(op)
-    }
-    console.log(values)
-
 
 
 }
 
 
 
+
+
+// RESETA TODOS OS VALORES DA CALCULADORA
+function reset() {
+    display.value = "";
+}
+
+// APAGA O ÚLTIMO DIGITO
+function backspace() {
+    let valor = display.value;
+    valor = valor.slice(0, -1);
+    display.value = valor;
+
+    let evento = new Event("keyup");
+    display.dispatchEvent(evento);
+}
+
+// RESOLVE A OPERAÇÃO
+function resolveOperation() {
+    valueChecker(values.operator)
+}
+
+
+
+// QUANDO UMA TECLA DE OPERAÇÃO É ACIONADA
+function enterOperation(op) {
+    // formatDisplay(op)
+    // if (typeof parseFloat(display.value) === "number") {
+    //     valueChecker(op)
+    // }
+    // console.log(values)
+    console.log(op)
+}
+
+// DIGITA O NÚMERO SELECIONADO NA CALCULADORA
+function enterNumber(value) {
+    console.log(value)
+    // display.focus();
+
+    // display.value += `${value}`;
+    // let evento = new Event("keyup");
+    // display.dispatchEvent(evento);
+}
+
+function teste() {
+    console.log("rubem")
+}
 
 // VERIFICA SE O DISPLAY ESTÁ APTO PARA CONTINUAR A OPERAÇÃO
 function formatDisplay(op) {
@@ -85,26 +125,25 @@ function formatDisplay(op) {
     }
 }
 
-function valueChecker(op){
-    if((display.value && display.value !== "+" && display.value !== "-") && !values.valueOne){
+
+// FAZ CHEGAGEM SE OS TEM VALORES PARA EFETUADA A OPERAÇÃO
+function valueChecker(op) {
+    if ((display.value && display.value !== "+" && display.value !== "-") && !values.valueOne) {
         values.valueOne = parseFloat(display.value);
         console.log(values.valueOne)
         reset()
         values.operator = op;
-    }else if((display.value && display.value !== "+" && display.value !== "-") && !values.valueTwo){
+    } else if ((display.value && display.value !== "+" && display.value !== "-") && !values.valueTwo) {
         values.valueTwo = parseFloat(display.value);
         reset()
     }
-    if(values.valueOne && values.valueTwo){
+    if (values.valueOne && values.valueTwo) {
         operation(values.valueOne, values.valueTwo, values.operator)
     }
 }
 
 
-function resolveOperation(op) {
-    valueChecker(values.operator)
-}
-
+// REALIZA A OPERAÇÃO
 function operation(num1, num2, op) {
     if (op == "+") {
         values.valueOperation = num1 + num2;
@@ -129,27 +168,6 @@ function operation(num1, num2, op) {
     }
 }
 
-// DIGITA O NÚMERO SELECIONADO NA CALCULADORA
-function enterNumber(value) {
-    display.focus();
-
-    display.value += `${value}`;
-    let evento = new Event("keyup");
-    display.dispatchEvent(evento);
-}
 
 
-// RESETA TODOS OS VALORES DA CALCULADORA
-function reset() {
-    display.value = "";
-}
-
-function backspace() {
-    let valor = display.value;
-    valor = valor.slice(0, -1);
-    display.value = valor;
-
-    let evento = new Event("keyup");
-    display.dispatchEvent(evento);
-}
 
